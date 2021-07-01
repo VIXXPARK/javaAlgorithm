14e095956a99156443f11afd88aeda07c974a3c8
Suhan Park <vixx170627@gmail.com>
2021-07-01 20:56:39 +0900

    two pointer

---
+++ leetcode/container_with_most_water/Solution.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int maxArea(int[] height) {
+        int left = 0, right = height.length - 1;
+        int ans = 0;
+        while (left <= right) {
+            int hi = 0;
+            int check = 0;
+            if (height[left] < height[right]) {
+                hi = height[left];
+                check = 0;
+            } else {
+                hi = height[right];
+                check = 1;
+            }
+            ans = Math.max(ans, hi * (right - left));
+            if (check == 1) {
+                right -= 1;
+            } else {
+                left += 1;
+            }
+        }
+        return ans;
+    }
+}
