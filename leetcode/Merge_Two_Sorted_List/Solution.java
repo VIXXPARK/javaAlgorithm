8ffae1c50101b1e85ba30e69d4dbd50c378067d9
VIXXPARK <vixx170627@gmail.com>
2021-07-08 08:41:42 +0900

    ListNode를 통한 합병정렬 문제 풀

---
+++ leetcode/Merge_Two_Sorted_List/Solution.java
@@ -0,0 +1,24 @@
+class Solution {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode head = new ListNode(0);
+        ListNode cur = head;
+        while (l1 != null && l2 != null) {
+            int left = l1.val;
+            int right = l2.val;
+            if (left > right) {
+                cur.next = l2;
+                l2 = l2.next;
+            } else {
+                cur.next = l1;
+                l1 = l1.next;
+            }
+            cur = cur.next;
+        }
+        if (l1 != null) {
+            cur.next = l1;
+        } else {
+            cur.next = l2;
+        }
+        return head.next;
+    }
+}
