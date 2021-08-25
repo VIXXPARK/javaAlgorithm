c3a7aeb43cef92a93a0e42610f87ecd3f6de40be
VIXXPARK <vixx170627@gmail.com>
2021-08-25 09:49:28 +0900

    hashmap

---
+++ programmers/전화번호목록/Solution.java
@@ -0,0 +1,18 @@
+package programmers.전화번호목록;
+import java.util.*;
+public class Solution {
+    public boolean solution(String[] phone_book){
+        Map<String,Boolean> map = new HashMap<>();
+        for(String number: phone_book){
+            map.put(number,true);
+        }
+        for(String number: phone_book){
+            for (int i = 0; i < number.length(); i++) {
+                String temp = number.substring(0,i);
+                if (map.containsKey(temp) && !temp.equals(number))
+                    return false;
+            }
+        }
+        return true;
+    }
+}
