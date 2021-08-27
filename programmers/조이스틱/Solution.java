536188550a497a7b9db2852a174a5436c12e27f2
VIXXPARK <vixx170627@gmail.com>
2021-08-27 14:39:42 +0900

    변수 이름의 중요성을 느꼈다.

---
+++ programmers/조이스틱/Solution.java
@@ -0,0 +1,25 @@
+package programmers.조이스틱;
+
+public class Solution {
+    public static void main(String[] args){
+        System.out.println(solution("ABBAAAAB"));
+    }
+    public static int solution(String name){
+        int answer = 0;
+        int[] diff = {0,1,2,3,4,5,6,7,8,9,10,11,12,13,12,11,10,9,8,7,6,5,4,3,2,1};
+        for (char c:name.toCharArray())
+            answer+=diff[c-'A'];
+
+        int length = name.length();
+        int min = length -1 ;
+        for (int i = 0; i < length; i++) {
+            int next = i+1;
+            while(next<length && name.charAt(next)=='A')
+                next++;
+            int BackStep = length-next;
+            int nextTargetStep = Math.min(i,length-next);
+            min = Math.min(min,i+BackStep+nextTargetStep);
+        }
+        return answer + min;
+    }
+}
