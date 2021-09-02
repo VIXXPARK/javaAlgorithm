256d26a941eecc793bdbe5a7781a8994f0c0c591
VIXXPARK <vixx170627@gmail.com>
2021-09-03 08:20:37 +0900

    sort하고 나서 해당 기준에 대하여 만족하는 값을 찾는 문제

---
+++ programmers/h_index/Solution.java
@@ -0,0 +1,24 @@
+package programmers.h_index;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Solution {
+    public int solution(int[] c) {
+        List<Integer> list = Arrays.stream(c).boxed().collect(Collectors.toList());
+        Collections.sort(list, Collections.reverseOrder());
+        int top = list.get(0);
+        int cur = 0;
+        while (true) {
+            while (cur < c.length && cur >= top) {
+                cur++;
+            }
+            if (cur >= top && c.length - cur <= top) {
+                return top;
+            }
+            top--;
+        }
+    }
+}
